f45884c068fc8512032581c0198d73c8e0fe1149
Strashisvkyi <55955717+Strashisvkyi@users.noreply.github.com>
2020-03-23 10:22:10 +0200

    added src for lab 6

---
+++ src/main/java/ua/lviv/iot/searchCredits/CreditSearcher.java
@@ -0,0 +1,25 @@
+package ua.lviv.iot.searchCredits;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CreditSearcher {
+	public static String findCredits(String line) {
+		String pattern = "(((\\d{4}\\s){4})|\\d{16})";
+		Pattern creditFinder = Pattern.compile(pattern);
+		Matcher matchingValue = creditFinder.matcher(line);
+		String credits = new String();
+		if (matchingValue.find()) {
+			do {
+				System.out.println("Found value: " + matchingValue.group(0));
+				credits=credits + matchingValue.group(0).trim()+" ";
+			} while (matchingValue.find());
+		} else {
+			System.out.println("No matches");
+		}
+		return credits;
+
+	}
+}
---
+++ src/test/java/ua/lviv/iot/searchCredits/CreditSearcherTest.java
@@ -0,0 +1,22 @@
+package ua.lviv.iot.searchCredits;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+
+public class CreditSearcherTest {
+	@Test
+	public void testCreditSearcher() {
+	
+    String input = "Diners Club 4567 8349 6573 1239 Discover 1122334455668495 Discover 6011000990139424 JCB 3530111333300000 JCB";
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in);
+    String foundCredits= CreditSearcher.findCredits(input);
+    assertEquals("4567 8349 6573 1239 "+"1122334455668495 "+"6011000990139424 "+"3530111333300000 ", foundCredits);
+}}
